d9aef9e0e2e3a32cd1da17e5461e6a681ba7cf8c
Thomas King <king.thomas.c@gmail.com>
2020-04-21 01:37:07 -0400

    feat(appRouter): using protectedRoute for protected routes

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Main, Login, OrderForm, ViewOrders } from '../components';
+import ProtectedRoute from './protectedRoute';
 
 const AppRouter = (props) => {
   return (
     <Router>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderForm} />
-      <Route path="/view-orders" exact component={ViewOrders} />
+      <ProtectedRoute path="/order" exact component={OrderForm} />
+      <ProtectedRoute path="/view-orders" exact component={ViewOrders} />
     </Router>
   );
 }
